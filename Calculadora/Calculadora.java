4935c0d66b775ae28b3a8e4e9971fae7b7428911
MJ <84123624+403rbidden@users.noreply.github.com>
2023-12-05 22:20:07 +0100

    Update Calculadora.java

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,55 +1,132 @@
-/**
- * Calculadora
- */
+import java.util.Scanner;
 
 public class Calculadora {
 
     public static void main(String[] args) {
 
-        // Definir variables para introducir los valores
-        float operador1 = 5;
-        float operador2 = 2;
-        // float operador2 = 0;
+        // Crear Scanner para la entrada del teclado
+        Scanner entradaTeclado = new Scanner(System.in);
 
-        // Definir variable para seleccionar la operación
-        int operacion = 5; // Cambiar el valor a un número entre 1 y 4 según la operación a ejecutar
+        // Declarar la var respuesta fuera del bucle para que su ambito se extienda a todo el cuerpo del metodo main
+        // Esto permite su uso en cualquier parte del metodo
+        // Char tipo primitivo para representar una unica letra
+        // Se tiene que inicializar la variable con un valor, aunque sea vacio, para que funcione
+        char respuesta = ' ';
 
-        //Calculadora
-        switch (operacion) {
-            case 1: // Suma
-                System.out.println("Resultado de la suma: " + (operador1 + operador2));
-                break;
+        // Bucle que permite ejecutar la calculadora repetidamente
+        do {
+            // ASCII art para la interfaz
+            String ASCIIArt = 
+                    "  ______________\n" +
+                    " | ____________ |\n" +
+                    " ||CALCULADORA ||\n" +
+                    " | \"\"\"\"\"\"\"\"\"\"\"\" |\n" +
+                    " |  [M|#|C][-]  |\n" +
+                    " |  [7|8|9][+]  |\n" +
+                    " |  [4|5|6][x]  |\n" +
+                    " |  [1|2|3][%]  |\n" +
+                    " |  [.|O|:][=]  |\n" +
+                    " |______________| By 403rbidden \n";
 
-            case 2: // Resta
-                System.out.println("Resultado de la resta: " + (operador1 - operador2));
-                break;
+            // Imprimir el ASCII art por consola
+            System.out.println(ASCIIArt);
 
-            case 3: // Multiplicación
-                System.out.println("Resultado de la multiplicación: " + (operador1 * operador2));
-                break;
+            // Solicitar el primer operador de la operacion
+            System.out.print("Introduce el primer operador: ");
 
-            case 4: // División
-                /*
-                if (operador2 == 0) {
-                    System.out.println("No se puede dividir por cero.");
+            // Verificar si la entrada operador1 es un numero
+            while (!entradaTeclado.hasNextFloat()) {
+                System.out.println("Error: Solo se permite introducir numeros. Intentalo de nuevo.");
+                // Limpiar el buffer del scanner antes de solicitar otro dato
+                entradaTeclado.next();
+                // Vuelve a solicitar el operador1
+                System.out.print("Introduce el primer operador: ");
+            }
+
+            float operador1 = entradaTeclado.nextFloat();
+
+            // Se utiliza para controlar el bucle que solicita el simbolo
+            // Mientras operacionValida sea falsa el bucle continuara hasta que se ingrese un simbolo correcto
+            boolean operacionValida = false;
+            
+            // Declarar operacionSimbolo fuera del bucle para que sea visible en todo el metodo
+            char operacionSimbolo = ' ';
+
+            // Bucle para seleccionar qué tipo de operacion se realizara
+            // No parara hasta que se introduzca un simbolo valido
+            while (!operacionValida) {
+                System.out.println("¿Qué operacion deseas realizar?\n" +
+                        "Escoge uno de los siguientes simbolos, segun sea necesario: \n" +
+                        "  [+] Sumar.\n" +
+                        "  [-] Restar.\n" +
+                        "  [*] Multiplicar.\n" +
+                        "  [/] Dividir.");
+
+                // Solicitar al usuario que ingrese el simbolo de operacion
+                System.out.print("Indica el simbolo: ");
+                operacionSimbolo = entradaTeclado.next().charAt(0);
+
+                // Validar si el simbolo de operacion ingresado es valido
+                if (operacionSimbolo == '+' || operacionSimbolo == '-' || operacionSimbolo == '*'
+                        || operacionSimbolo == '/') {
+                    operacionValida = true;
                 } else {
-                    System.out.println("Resultado de la división: " + (operador1 / operador2));
+                    System.out.println("Simbolo no reconocido. Por favor, selecciona uno valido.");
                 }
-                break;
-                */
+            }
 
-                System.out.println((operador2 == 0) ? "No se puede dividir por cero." : "Resultado de la división: " + (operador1 / operador2));
-                /**
-                 * Operador ternario
-                 * (condición) ? expresión_si_verdadero : expresión_si_falso;
-                 */
-                break;
-        
-            default: // Otro valor no contemplado
-                System.out.println("Esta operación no se puede realizar.");
-                break;
-        }
+            // Solicitar el segundo operador de la operacion
+            System.out.print("Introduce el segundo operador: ");
 
-        System.out.println("Fin del programa, ¡gracias!");
+            // Verificar si operador2 es un numero
+            while (!entradaTeclado.hasNextFloat()) {
+                System.out.println("Error: Solo se permite introducir numeros. Intentalo de nuevo.");
+                // Limpiar el buffer del scanner antes de solicitar otro dato
+                entradaTeclado.next();
+                // Vuelve a solicitar el operador2
+                System.out.print("Introduce el segundo operador: ");
+            }
+
+            float operador2 = entradaTeclado.nextFloat();
+
+            // Inicializar resultado = 0 asegura que tenga un valor definido antes realizar otra operacion
+            float resultado = 0;
+
+            // Realizar la operacion de la calculadora utilizando un switch
+            switch (operacionSimbolo) {
+                case '+':
+                    resultado = operador1 + operador2;
+                    break;
+
+                case '-':
+                    resultado = operador1 - operador2;
+                    break;
+
+                case '*':
+                    resultado = operador1 * operador2;
+                    break;
+
+                case '/':
+                    resultado = operador1 / operador2;
+                    break;
+            }
+
+            // Mostrar el resultado de la operacion, manejando la division por cero
+            System.out.println((operacionSimbolo == '/' && operador2 == 0) ? "Error: Division por cero no permitida."
+                    : "El resultado de la operacion es = " + resultado);
+
+            // Preguntar al usuario si desea realizar otra operacion
+            System.out.println("¿Desea realizar otra operacion?");
+            System.out.print("Pulse [S] para si y [N] para salir del programa: ");
+            respuesta = entradaTeclado.next().charAt(0);
+
+        } while (Character.toUpperCase(respuesta) == 'S');
+
+        // Mensaje de despedida
+        System.out.println("Gracias por usar esta calculadora. \n" +
+                "¡Hasta pronto! :)");
+
+        // Cerrar el objeto Scanner al terminar para evitar fugas de recursos
+        entradaTeclado.close();
     }
 }
